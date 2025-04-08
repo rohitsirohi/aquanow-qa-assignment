6f230410b294eaf5d712a4af8f274acb3273b35c
Rohit Sirohi <rohit.singh.sirohi@gmail.com>
2025-04-08 12:22:20 -0700

    Added soft assertions

--- utils/api-assertions.js
+++ utils/api-assertions.js
@@ -1,15 +1,15 @@
 import { expect } from '@playwright/test'
 
 export function assertThatResponseIsOk(response) {
-  expect(response.ok()).toBeTruthy()
+  expect.soft(response.ok()).toBeTruthy()
 }
 
 export function assertResponseStatus(response, status) {
-  expect(response.status()).toBe(status)
+  expect.soft(response.status()).toBe(status)
 }
 
 export function assertResponseErrorMessage(responseBody, errorMessage) {
-  expect(responseBody.error).toHaveProperty('message', errorMessage)
+  expect.soft(responseBody.error).toHaveProperty('message', errorMessage)
 }
 
 export function assertResponseBodyHasProperty(
@@ -17,6 +17,6 @@ export function assertResponseBodyHasProperty(
   jsonObjectProperties
 ) {
   for (const [key, value] of Object.entries(jsonObjectProperties)) {
-    expect(responseBody).toHaveProperty(key, value)
+    expect.soft(responseBody).toHaveProperty(key, value)
   }
 }
